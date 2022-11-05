276248f3e9aab550318d69dafe2280b5277e25f8
Paul Schlack <paul.schlack@me.com>
2022-11-05 15:33:51 +0100

    fixd

--- models.py
+++ models.py
@@ -67,8 +67,8 @@ class DepthwiseSeperableConv3D(nn.Module):
     '''
     def __init__(self, in_channels, out_channels, kernel_size, stride=1, padding=0, dilation=1) -> None:
         super().__init__()
-        self.depthwise_conv = nn.Conv3d(in_channels=in_channels, out_channels=out_channels, kernel_size=kernel_size, stride=stride, padding=padding, dilation=dilation, groups=out_channels)
-        self.pointwise_conv = nn.Conv3d(in_channels=out_channels, out_channels=out_channels, kernel_size=1)
+        self.depthwise_conv = nn.Conv3d(in_channels=in_channels, out_channels=in_channels, kernel_size=kernel_size, stride=stride, padding=padding, dilation=dilation, groups=in_channels)
+        self.pointwise_conv = nn.Conv3d(in_channels=in_channels, out_channels=out_channels, kernel_size=1)
 
     def forward(self, x):
         x = self.depthwise_conv(x)
